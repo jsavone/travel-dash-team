964cd3b178529527f88da1728f11a4e937e2c883
jsavone <john.savone@gmail.com>
2018-05-31 11:40:36 -0700

    newst version, before CSS

--- controllers/airline.js
+++ controllers/airline.js
@@ -3,6 +3,8 @@ const knex = require("../db/knex.js");
 module.exports = {
   // CHANGE ME TO AN ACTUAL FUNCTION
   view: function(req, res) {
-    res.send("Hello");
+    knex('airline').join('trip', 'airline.id', 'trip.airline_id').where('airline.id', req.params.id).where('trip.user_id', req.session.user_id).then((airline) =>{
+      res.render('airline', {trips: airline})
+    })
   },
 }
